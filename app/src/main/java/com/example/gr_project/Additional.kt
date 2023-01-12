af61b1b466410687134807044c66d09a27423cc7
herald27701 <quochuy181013@gmail.com>
2023-01-12 10:06:28 +0700

    First Hello World 123

--- app/src/main/java/com/example/gr_project/Additional.kt
+++ app/src/main/java/com/example/gr_project/Additional.kt
@@ -1,4 +1,5 @@
 package com.example.gr_project
 
 class Additional {
+
 }
--- app/src/main/java/com/example/gr_project/MainActivity.kt
+++ app/src/main/java/com/example/gr_project/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val txt: String = "Hello world!"
+        val num: Int = 123
     }
 }
